770e753dff650e106c12d02ee9f14d5c060ce446
slacken <zengbin.zb@alibaba-inc.com>
2018-06-22 17:32:34 +0800

    v1.0.2

--- lib/batches.rb
+++ lib/batches.rb
@@ -4,12 +4,12 @@ class Batches
   class NoBlockError < ArgumentError; end
   
   def self.do(cocurrence, entities, &block)
-    raise NoBlockError, "No block given" unless block
-    entities.each_slice(cocurrence) do |group|
+    raise NoBlockError, "No block given" if block.nil?
+    entities.each_slice(cocurrence).map do |group|
       threads = group.map do |entity|
         Thread.new(entity){|e| block.call(e) }
       end
-      threads.map{|t| t.join }
+      threads.map{|t| t.value }
     end.flatten(1)
   end
 end
